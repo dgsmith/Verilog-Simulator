0a2c9cdc23dff67d4e80bc597c0862e210269919
Grayson Smith <d.grayson.smith@gmail.com>
2012-04-09 13:10:09 -0700

    working on parser

--- parser.cpp
+++ parser.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <ostream>
-//#include "design.h"
+#include "design.h"
+#include "gate.h"
+#include "net.h"
 
 using namespace std;
 
@@ -14,8 +16,35 @@ Design *parseThatShit(string ifile)
 		string currentline;
 		getline(ifile, currentline);
 		stringstream ss(currentline);
+		if(!ss.good())
+		{
+			// no text here yo
+			continue;
+		}
 		string token;
 		ss >> token;
-		cout << token << endl;
+		if(token == "//")
+			// disregard comments
+			continue;
+		if(token == "module")
+		{
+			// start of design
+			// get the name first
+			string next_token;
+			getline(ss, next_token, "(");
+			string module_name;
+			next_token >> module_name;
+			// now get the list of ports
+			string port_list;
+			getline(ss, port_list, ")");
+			// parse the list for inputs and outputs
+			stringstream port_list(port_list);
+			while(!port_list.fail()) // will run even if eof bit is set!!
+			{
+				string current_port;
+				getline(port_list,current_port,",");
+				
+			}
+		}
 	}
 }
