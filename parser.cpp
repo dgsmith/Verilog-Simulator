90bcc983f036c3b17b3937e132409771a1ec4588
Grayson Smith <d.grayson.smith@gmail.com>
2012-04-16 20:58:09 -0700

    working on parser

--- parser.cpp
+++ parser.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ostream>
+#include <string>
 #include <regex>
 #include "design.h"
 #include "gate.h"
@@ -7,42 +8,61 @@
 
 using namespace std;
 
-#define SEMI ";"
-#define ID "[A-Za-z_][A-Za-z0-9_]"
+enum {COMMENT, BLANK, MODULE, INPUT, OUTPUT, WIRE, GATE, END, ERROR};
+	
+#define ID "[A-Za-z_][A-Za-z0-9_]*"
 #define NUM "[0-9]+"
-#define LEFTPARAM "("
-#define RIGHTPARAM ")"
-#define COMMA ","
-#define POUND "#"
-#define INPUT "input"
-#define OUTPUT "output"
-#define WIRE "wire"
-#define MODULE "module"
-#define ENDMODULE "endmodule"
-#define GATETYPE "[and]|[or]|[nand]|[nor]|[xor]|[not]"
+#define GATE_TYPE ""and" | "or" | "nand" | "nor" | "xor" | "not""
 
-Design *parseThatShit(string ifile);
-
-void match()
+int lineType(string currentline);
+string getID(string characters);
+void parenParser(vector<string> *ports, string input);
 
 Design *parseThatShit(string ifile)
 {
+	string designName;
+	vector<string> port_list;
+	
 	ifstream ifile(ifile, ifstream::in);
 	while(ifile.good())
 	{
 		string currentline;
 		getline(ifile, currentline);
 		stringstream ss(currentline);
-		if(!ss.good())
+		string firsttoken;
+		ss >> firsttoken;
+		switch(lineType(firsttoken))
 		{
-			// no text here yo
-			continue;
+			case COMMENT :
+				// comment line
+				continue;
+			case BLANK :
+				
+				continue;
+			case MODULE :
+				// line defining a module
+				string possibleID;
+				getline(ss ,possibleID, "(");
+				designName = getID(possibleID); // get name 
+				
+				string possiblePorts;
+				getline(ss, possiblePorts, ")");
+				parenParser(port_list, possiblePorts);
+				
+				continue;
+			case INPUT :
+				// input line
+			case OUTPUT :
+				
+			case WIRE :
+				
+			case GATE :
+				
+			case END :
+				
+			case ERROR:
+				
 		}
-		string token;
-		ss >> token;
-		if(token == "//")
-			// disregard comments
-			continue;
 		if(token == "module")
 		{
 			// start of design
@@ -63,5 +83,66 @@ Design *parseThatShit(string ifile)
 				
 			}
 		}
+	}
+}
+
+int lineType(string identifier) // going line by line, so decide what kind of line this is
+{
+
+	if(identifier == "//")
+	{
+		return COMMENT;
+	}
+	else if(identifier == "module")
+	{
+		return MODULE;
+	}
+	else if(identifier == "input")
+	{
+		return INPUT;
+	}
+	else if(identifier == "output")
+	{
+		return OUTPUT;
+	}
+	else if(identifier == "wire")
+	{
+		return WIRE;
+	}
+	else if((identifier == "and") | (identifier == "or") | (identifier == "nand") | (identifier == "nor") | (identifier == "xor") | (identifier == "not"))
+	{
+		return GATE;
+	}
+	else if(identifier == "endmodule")
+	{
+		return END;
+	}
+	else
+	{
+		return ERROR;
+	}
+}
+
+string getID(string input)
+{
+	regex rgx(ID);
+	smatch result;
+	regex_search(input, result, rgx);
+	return result.str();
+}
+
+void parenParser(vector<string>* ports, string input)
+{
+	stringstream ss(input);
+	while(!ss.fail()) // runs last time when eof bit is set!
+	{
+		if(ss.eof())	{ // time to grab last num and leave!
+			ss.unget();
+			ports.push_back(ss.get());
+			break;
+		}
+		string port;
+		getline(ss, port, ",");
+		ports.push_back(port);
 	}
 }
