0b58481d0c0345861a38cc182ab5dc6aad52fc38
Chris <licy@usc.edu>
2012-04-18 23:51:07 -0700

    line numbers

--- gatesim.cpp
+++ gatesim.cpp
@@ -1,12 +1,18 @@
+/**
+* PA5 Part 1
+* Chris Li, Grayson Smith
+*/
 #include "global.h"
 #include <iostream>
 #include "parser.h"
 
 int main (int argc, char const *argv[])
 {
-  LOG("Hi");
+  LOG("Parsing and dumping test.v");
 	Design *aDesign;
-	aDesign = parseThatShit("test2.v");
-  LOG("BYE");
+	aDesign = parseThatShit("test.v");
+  
+  LOG("Parsing and dumping test2.v");
+  aDesign = parseThatShit("test2.v");
   return 0;
 }
